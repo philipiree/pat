9be2a2a80152694a9b80275bcf1575903f95bbf2
Philip Salas <philipiresalas@gmail.com>
2020-02-03 14:22:25 +0800

    3rd commit

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -37,12 +37,6 @@
                             <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/about">About</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link " href="/services">Services</a>
-                        </li>
-                        <li class="nav-item">
                             <a class="nav-link " href="/posts">Blog</a>
                         </li>
                     </ul>
